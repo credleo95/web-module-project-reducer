2057bae0a8f3e9ed8f8b39cdaea50ced14fb0fe1
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-19 23:05:51 -0400

    connected add one dispatch to button 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useReducer } from 'react';
-
+import {addOne, ADD_ONE} from './actions/index'; 
 import './App.css';
 import reducer, { initialState } from './reducers/index';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
-const [state, dispatch] = useReducer(reducer, {initialState});
+const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -17,10 +18,10 @@ const [state, dispatch] = useReducer(reducer, {initialState});
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,7 +31,7 @@ const [state, dispatch] = useReducer(reducer, {initialState});
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={() => {dispatch({type: ADD_ONE})}}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
